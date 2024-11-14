90843c452803d47232358b13984017dbaa64067e
xianfengsun <xianfengsun@192.168.0.111>
2024-11-14 22:57:12 +0800

    delete unused files

--- AttelasWebApi/obj/Debug/net8.0/AttelasWebApi.AssemblyInfo.cs
+++ AttelasWebApi/obj/Debug/net8.0/AttelasWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AttelasWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+700605046ab9dee3eb4ff81ec05e335672545e16")]
 [assembly: System.Reflection.AssemblyProductAttribute("AttelasWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AttelasWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
